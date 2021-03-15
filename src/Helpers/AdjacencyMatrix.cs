6ae95a4f1922ae3105401afa17626a3706129acc
Petr Kovalev <pkovalev@os33.com>
2021-03-15 10:28:42 +0000

    AdjacencyMatrix improved

--- src/Helpers/AdjacencyMatrix.cs
+++ src/Helpers/AdjacencyMatrix.cs
@@ -1,45 +1,43 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using PunchedCards.BitVectors;
 
 namespace PunchedCards.Helpers
 {
     internal sealed class AdjacencyMatrix
     {
-        internal AdjacencyMatrix(IReadOnlyCollection<IBitVector> bitVectors)
+        internal AdjacencyMatrix(IEnumerable<IBitVector> bitVectors)
         {
-            Size = bitVectors.First().Count;
-            Matrix = new int[Size, Size];
-            PopulateAdjacencyMatrix(Matrix, bitVectors, out var halfSum);
+            Matrix = PopulateAdjacencyMatrix(bitVectors, out var halfSum);
             HalfSum = halfSum;
         }
 
         internal int[,] Matrix { get; }
 
-        internal int Size { get; }
+        internal int Size => Matrix.GetLength(0);
 
         internal long HalfSum { get; }
 
-        private static void PopulateAdjacencyMatrix(
-            int[,] matrix,
-            IEnumerable<IBitVector> bitVectors,
-            out long halfSum)
+        private static int[,] PopulateAdjacencyMatrix(IEnumerable<IBitVector> bitVectors, out long halfSum)
         {
+            int[,] adjacencyMatrix = null;
             halfSum = 0;
 
             foreach (var bitVector in bitVectors)
             {
+                adjacencyMatrix ??= new int[bitVector.Count, bitVector.Count];
+
                 var activeBitIndices = bitVector.ActiveBitIndices;
-                var numberOfOccurrences = 1;
                 for (var i = 0; i < activeBitIndices.Count; i++)
                 {
                     for (var j = i; j < activeBitIndices.Count; j++)
                     {
-                        matrix[activeBitIndices[i], activeBitIndices[j]] += numberOfOccurrences;
-                        halfSum += numberOfOccurrences;
+                        adjacencyMatrix[activeBitIndices[i], activeBitIndices[j]]++;
+                        halfSum++;
                     }
                 }
             }
+
+            return adjacencyMatrix;
         }
     }
 }
