6882f87d315b468aaa35e898d90dd2e504c34714
Petr Kovalev <pkovalev@os33.com>
2022-08-14 12:57:26 +0300

    Code cleanup

--- src/Helpers/RecognitionHelper.cs
+++ src/Helpers/RecognitionHelper.cs
@@ -21,16 +21,16 @@ namespace PunchedCards.Helpers
                 .AsParallel()
                 .ForAll(dataItem =>
                 {
-                    var matchingScores = CalculateMatchingScores(dataItem.Item1, expertsPerKey, puncher).ToList();
-                    var matchingScoresPerLabel = CalculateMatchingScoresPerLabel(matchingScores, bitVectorFactory, topPunchedCardsCount);
-                    var topLabel = matchingScoresPerLabel
-                        .MaxBy(p => p.Value)
-                        .Key;
+                    var matchingScoresPerKey = CalculateMatchingScoresPerKey(dataItem.Item1, expertsPerKey, puncher).ToList();
+                    var topMatchingScoresPerKey = GetTopMatchingScoresPerKey(matchingScoresPerKey, topPunchedCardsCount);
+                    var topMatchingScoresPerLabel = CalculateMatchingScoresPerLabel(topMatchingScoresPerKey, bitVectorFactory);
 
+                    var topLabel = topMatchingScoresPerLabel.MaxBy(p => p.Value).Key;
                     if (topLabel.Equals(dataItem.Item2))
                     {
                         Interlocked.Increment(ref counters[topLabel][0]);
                     }
+
                 });
 
             return counters.ToDictionary(p => p.Key, p => p.Value[0]);
@@ -45,29 +45,35 @@ namespace PunchedCards.Helpers
                 .ToDictionary(tuple => tuple.Item1, tuple => tuple.Item2);
         }
 
-        private static IEnumerable<KeyValuePair<IBitVector, double>> CalculateMatchingScoresPerLabel(
-            IReadOnlyCollection<IReadOnlyDictionary<IBitVector, double>> matchingScoresCollection,
-            IBitVectorFactory bitVectorFactory,
-            int? topPunchedCardsCount)
-        {
-            var topMatchingScores =
-                !topPunchedCardsCount.HasValue
-                    ? matchingScoresCollection
-                    : matchingScoresCollection
-                        .OrderByDescending(p => p.Values.Max())
-                        .Take(topPunchedCardsCount.Value)
-                        .ToList();
-
-            return DataHelper.GetLabels(bitVectorFactory).Select(label =>
-                KeyValuePair.Create(label, topMatchingScores.Sum(matchingScores => matchingScores[label])));
-        }
-
-        private static IEnumerable<IReadOnlyDictionary<IBitVector, double>> CalculateMatchingScores(
+        private static IEnumerable<KeyValuePair<string, IReadOnlyDictionary<IBitVector, double>>> CalculateMatchingScoresPerKey(
             IBitVector bitVector,
             IEnumerable<KeyValuePair<string, IExpert>> expertsPerKey,
             IPuncher<string, IBitVector, IBitVector> puncher)
         {
-            return expertsPerKey.Select(expertPerKey => expertPerKey.Value.CalculateMatchingScores(puncher.Punch(expertPerKey.Key, bitVector).Input));
+            return expertsPerKey.Select(expertPerKey => KeyValuePair.Create(
+                expertPerKey.Key,
+                expertPerKey.Value.CalculateMatchingScores(puncher.Punch(expertPerKey.Key, bitVector).Input)));
+        }
+
+        private static IReadOnlyCollection<KeyValuePair<string, IReadOnlyDictionary<IBitVector, double>>> GetTopMatchingScoresPerKey(
+            IReadOnlyCollection<KeyValuePair<string, IReadOnlyDictionary<IBitVector, double>>> matchingScoresPerKey,
+            int? topPunchedCardsCount)
+        {
+            return !topPunchedCardsCount.HasValue
+                    ? matchingScoresPerKey
+                    : matchingScoresPerKey
+                        .OrderByDescending(p => p.Value.Values.Max())
+                        .Take(topPunchedCardsCount.Value)
+                        .ToList();
+        }
+
+        private static IEnumerable<KeyValuePair<IBitVector, double>> CalculateMatchingScoresPerLabel(
+            IReadOnlyCollection<KeyValuePair<string, IReadOnlyDictionary<IBitVector, double>>> topMatchingScoresPerKey,
+            IBitVectorFactory bitVectorFactory)
+        {
+            return DataHelper.GetLabels(bitVectorFactory).Select(label => KeyValuePair.Create(
+                label,
+                topMatchingScoresPerKey.Sum(matchingScores => matchingScores.Value[label])));
         }
     }
 }
