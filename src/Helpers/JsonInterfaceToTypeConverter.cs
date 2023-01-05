788486ce2730a5a44f5c70cceb299656437292f6
Petr Kovalev <pkovalev@os33.com>
2023-01-06 00:53:01 +0300

    Added JsonSerializer

---
+++ src/Helpers/JsonInterfaceToTypeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Runtime;
+using PunchedCards.BitVectors;
+
+namespace PunchedCards.Helpers
+{
+	internal sealed class JsonInterfaceToTypeConverter : JsonConverter
+    {
+        private static readonly IDictionary<string, Type> Mapping = new Dictionary<string, Type>(StringComparer.OrdinalIgnoreCase);
+
+        static JsonInterfaceToTypeConverter()
+        {
+            Map<IBitVector, BitVector>();
+            Map<IExpert, Expert>();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return Mapping.TryGetValue(objectType.Name, out _);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            Newtonsoft.Json.JsonSerializer serializer)
+        {
+            return Mapping.TryGetValue(objectType.Name, out var type) ? serializer.Deserialize(reader, type) : null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+        {
+            serializer.Serialize(writer, value);
+        }
+
+        private static void Map<TI, T>() where T : class, TI
+        {
+            Mapping.Add(typeof(TI).Name, typeof(T));
+        }
+    }
+}
---
+++ src/Helpers/JsonSerializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PunchedCards.Helpers
+{
+	internal sealed class JsonSerializer
+	{
+        private const int DefaultBufferSize = 80 * 1024; // 80K is the value from modern stream extension method CopyTo implementation
+
+        private static readonly Encoding Utf8WithoutBom = new UTF8Encoding(false);
+
+        private readonly Newtonsoft.Json.JsonSerializer _jsonSerializer;
+
+        private JsonSerializer()
+		{
+            _jsonSerializer = Newtonsoft.Json.JsonSerializer.Create(new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters =
+                {
+                    new JsonInterfaceToTypeConverter()
+                }
+            });
+        }
+
+		internal static JsonSerializer Instance { get; } = new JsonSerializer();
+
+        internal void Serialize(object value, StreamWriter streamWriter)
+        {
+            using (var jsonTextWriter = new JsonTextWriter(streamWriter)
+            {
+                CloseOutput = false,
+                AutoCompleteOnClose = false
+            })
+            {
+                _jsonSerializer.Serialize(jsonTextWriter, value);
+            }
+        }
+
+        internal T Deserialize<T>(StreamReader streamReader)
+        {
+            using (var jsonTextReader = new JsonTextReader(streamReader)
+            {
+                CloseInput = false
+            })
+            {
+                return _jsonSerializer.Deserialize<T>(jsonTextReader);
+            }
+        }
+    }
+}
+
