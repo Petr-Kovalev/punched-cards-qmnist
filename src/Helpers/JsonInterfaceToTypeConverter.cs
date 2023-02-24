8ec02834e862f99002080c4f701c7a3560f0a4ae
Petr Kovalev <pkovalev@os33.com>
2023-02-24 23:00:52 +0300

    Code cleanup

--- src/Helpers/JsonInterfaceToTypeConverter.cs
+++ src/Helpers/JsonInterfaceToTypeConverter.cs
@@ -8,13 +8,11 @@ namespace PunchedCards.Helpers
 {
     internal sealed class JsonInterfaceToTypeConverter : JsonConverter<object>
     {
-        private static readonly IDictionary<Type, Type> Mapping = new Dictionary<Type, Type>();
-
-        static JsonInterfaceToTypeConverter()
+        private static readonly IReadOnlyDictionary<Type, Type> Mapping = new Dictionary<Type, Type>()
         {
-            Map<IBitVector, BitVector>();
-            Map<IExpert, Expert>();
-        }
+            { typeof(IBitVector), typeof(BitVector) },
+            { typeof(IExpert), typeof(Expert) }
+        };
 
         public override bool CanConvert(Type typeToConvert)
         {
@@ -23,17 +21,12 @@ namespace PunchedCards.Helpers
 
         public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return Mapping.TryGetValue(typeToConvert, out var type) ? System.Text.Json.JsonSerializer.Deserialize(ref reader, type, options) : null;
+            return Mapping.TryGetValue(typeToConvert, out var returnType) ? System.Text.Json.JsonSerializer.Deserialize(ref reader, returnType, options) : null;
         }
 
         public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
         {
             System.Text.Json.JsonSerializer.Serialize(writer, value, options);
-        }
-
-        private static void Map<TI, T>() where T : class, TI
-        {
-            Mapping.Add(typeof(TI), typeof(T));
         }
     }
 }
