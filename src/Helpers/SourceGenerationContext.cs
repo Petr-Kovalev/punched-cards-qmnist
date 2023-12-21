01b53751754ef67b96027663ae6f516bf2880fc1
Petr Kovalev <pkovalev@os33.com>
2023-12-21 23:57:44 +0300

    AOT serialization fixes

--- src/Helpers/SourceGenerationContext.cs
+++ src/Helpers/SourceGenerationContext.cs
@@ -3,8 +3,11 @@ using System.Text.Json.Serialization;
 
 namespace PunchedCards.Helpers
 {
+    [JsonSerializable(typeof(IBitVector))]
     [JsonSerializable(typeof(BitVector))]
     [JsonSerializable(typeof(IReadOnlyList<ValueTuple<IBitVector, IBitVector>>))]
+    [JsonSerializable(typeof(List<ValueTuple<IBitVector, IBitVector>>))]
+    [JsonSerializable(typeof(IExpert))]
     [JsonSerializable(typeof(Expert))]
     [JsonSerializable(typeof(IReadOnlyDictionary<string, IExpert>))]
     internal partial class SourceGenerationContext : JsonSerializerContext;
